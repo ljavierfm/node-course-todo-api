7895c961a9da98a1dbffe54ce2a2efd9fd1470b1
Javier Fernandez <ljavierfm@gmail.com>
2017-11-09 20:25:20 +0100

    Ruta /post

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
 //usar promise
 mongoose.Promise=global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp',{useMongoClient:true});
 
 module.exports={
-    mongoose=mongoose
+    mongoose:mongoose
 };
--- server/server.js
+++ server/server.js
@@ -7,6 +7,21 @@ var { User } = require('./models/user.js');
 
 var app=express();
 
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    var todo=new Todo({
+        text:req.body.text
+    });
+    
+    todo.save().then((document)=>{
+        res.send(document);
+    },(err)=>{
+        res.status(400).send(err);
+    });
+
+});
+
 app.listen(3000,()=>{
     console.log('Started on port 3000');
 });
